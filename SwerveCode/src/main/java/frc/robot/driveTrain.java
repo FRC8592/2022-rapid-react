fdcfa46971d0bf321ec49e7467446e8d4b550d98
bob <ols.hedgehog@gmail.com>
2021-12-03 20:21:06 -0500

    Swerve Drive Subsystems added to driveTrain module

--- SwerveCode/src/main/java/frc/robot/driveTrain.java
+++ SwerveCode/src/main/java/frc/robot/driveTrain.java
@@ -6,6 +6,11 @@ import edu.wpi.first.wpilibj.XboxController; //this puts in the xbox contoller s
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+
 import com.swervedrivespecialties.swervelib.Mk3SwerveModuleHelper;
 import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
 import com.swervedrivespecialties.swervelib.SwerveModule;
@@ -34,6 +39,45 @@ public class driveTrain {
 	// Differential drive class
   DifferentialDrive robotDrive;
 
+  /**
+   * The maximum voltage that will be delivered to the drive motors.
+   * <p>
+   * This can be reduced to cap the robot's maximum speed. Typically, this is useful during initial testing of the robot.
+   */
+  public static final double MAX_VOLTAGE = 12.0;
+  // FIXME Measure the drivetrain's maximum velocity or calculate the theoretical.
+  //  The formula for calculating the theoretical maximum velocity is:
+  //   <Motor free speed RPM> / 60 * <Drive reduction> * <Wheel diameter meters> * pi
+  //  By default this value is setup for a Mk3 standard module using Falcon500s to drive.
+  //  An example of this constant for a Mk4 L2 module with NEOs to drive is:
+  //   5880.0 / 60.0 / SdsModuleConfigurations.MK4_L2.getDriveReduction() * SdsModuleConfigurations.MK4_L2.getWheelDiameter() * Math.PI
+  /**
+   * The maximum velocity of the robot in meters per second.
+   * <p>
+   * This is a measure of how fast the robot should be able to drive in a straight line.
+   */
+  public static final double MAX_VELOCITY_METERS_PER_SECOND = 6380.0 / 60.0 *
+          SdsModuleConfigurations.MK3_STANDARD.getDriveReduction() *
+          SdsModuleConfigurations.MK3_STANDARD.getWheelDiameter() * Math.PI;
+  /**
+   * The maximum angular velocity of the robot in radians per second.
+   * <p>
+   * This is a measure of how fast the robot can rotate in place.
+   */
+  // Here we calculate the theoretical maximum angular velocity. You can also replace this with a measured amount.
+  public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
+          Math.hypot(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0);
+
+  private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
+          // Front left
+          new Translation2d(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0),
+          // Front right
+          new Translation2d(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, -DRIVETRAIN_WHEELBASE_METERS / 2.0),
+          // Back left
+          new Translation2d(-DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0),
+          // Back right
+          new Translation2d(-DRIVETRAIN_TRACKWIDTH_METERS / 2.0, -DRIVETRAIN_WHEELBASE_METERS / 2.0)
+  );
 
   /**
    * Initialize drivetrain
@@ -101,19 +145,19 @@ public class driveTrain {
     // Read gamepad controls
     forward = driveTrainController.getTriggerAxis(GenericHID.Hand.kRight);  // Right trigger
     reverse = driveTrainController.getTriggerAxis(GenericHID.Hand.kLeft);   // Left Trigger
-    turn    = driveTrainController.getX(GenericHID.Hand.kLeft);             // Left joystick
+    turn    = driveTrainController.getX(GenericHID.Hand.kLeft) * MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND;             // Left joystick
     //
     reverseControl = driveTrainController.getBButton();                      // B button
     curveOff       = driveTrainController.getBumper(GenericHID.Hand.kRight); // Right bumper
   
     // Combine and scale inputs
-    throttle = (-forward + reverse) * DRIVE_POWER;
+    throttle = (-forward + reverse) * MAX_VELOCITY_METERS_PER_SECOND;
 
     // Apply a different power curve for turn-in-place
     if (!curveOff)
-      turn = turn * TURN_POWER;
+      turn = turn * MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND;
     else
-      turn = turn * TURN_IN_PLACE_POWER;
+      turn = turn * MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND;
 
     // If reverseControl is being pressed, invert all inputs so the robot can be driven backwards
     if (reverseControl) {
@@ -121,15 +165,28 @@ public class driveTrain {
     }
     
     // Send controls to the robot drive system
-    robotDrive.curvatureDrive(throttle, turn, curveOff);
+    
+    drive(new ChassisSpeeds(throttle, 0.0, turn));
   }
 
   public void autoDrive(){
-    robotDrive.curvatureDrive(0.3, 0, false);
+    ChassisSpeeds chassisSpeeds = new ChassisSpeeds(0.3, 0.0, 0.0);
+    drive(chassisSpeeds);
   }
 
   public void driveStop(){
-    robotDrive.curvatureDrive(0, 0, false);
+    ChassisSpeeds chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
+    drive(chassisSpeeds);
   }
 
+  public void drive(ChassisSpeeds chassisSpeeds) {
+
+    SwerveModuleState[] states = m_kinematics.toSwerveModuleStates(chassisSpeeds);
+    SwerveDriveKinematics.normalizeWheelSpeeds(states, MAX_VELOCITY_METERS_PER_SECOND);
+
+    m_frontLeftModule.set(states[0].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[0].angle.getRadians());
+    m_frontRightModule.set(states[1].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[1].angle.getRadians());
+    m_backLeftModule.set(states[2].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[2].angle.getRadians());
+    m_backRightModule.set(states[3].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[3].angle.getRadians());
+  }
 }
