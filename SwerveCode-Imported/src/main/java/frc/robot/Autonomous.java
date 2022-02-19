22b31785d7a8f6b14c0823db6875250bf28a94c0
AShch06 <ols.hedgehog@gmail.com>
2022-02-18 21:03:07 -0500

    added collector state machine
    This added the collector state machine which reads IR sensors and runs
    different wheels based of off that. Also changed class names to all be
    capital.

--- SwerveCode-Imported/src/main/java/frc/robot/Autonomous.java
+++ SwerveCode-Imported/src/main/java/frc/robot/Autonomous.java
@@ -7,12 +7,13 @@ public class Autonomous {
     private enum AutoState{SHOOT, DRIVE, STOP};
     private AutoState currentState = AutoState.SHOOT;
     private Timer autonomousTimer;
-    private driveTrain drive;
+    private Drivetrain drive;
     private Collector collector;
     
-    public Autonomous(driveTrain drive) {
+    public Autonomous(Drivetrain drive) {
         this.drive = drive;
         autonomousTimer = new Timer();
+        collector = new Collector();
 
         autonomousTimer.start();
 
@@ -44,23 +45,5 @@ public class Autonomous {
     }
 
 
-    public void ballControl(boolean IR1, boolean IR2){
-        //Note: Ideally this would be in a switch statement but I haven't quite figured out how to do this yet. Maybe throught the use of an array or list??
-        boolean oneBallBottom = IR1 && !IR2;
-        boolean oneBallTop = !IR1 && IR2;
-        boolean twoBallsCollected = IR1 && IR2;
-        boolean noBallsCollected = !IR1 && !IR2;
-        
-            if(noBallsCollected){
-                collector.driveProcessingWheels(); //if we don't have any balls we would like to run the processing wheels
-            }else if(oneBallBottom){
-                while(oneBallBottom || noBallsCollected){   //WARNING: WHILE LOOPS ARE BAD FIX LATER
-                    collector.driveStagingWheels(); //if we go between the two motors we might go into an no balls condition meaning that in this specific if we want to run both when the ball is at 1 ball bottom and while we dont have any balls
-                }
-            }else if(oneBallTop){
-                collector.driveProcessingWheels(); //We want to run the staging wheels while the ball is at the top of the collector
-            }else{
-                collector.intakeAllStop(); //This is the only option left which is 2 balls collected unless there is manual control going on
-            }
-    }
+
 }
--- SwerveCode-Imported/src/main/java/frc/robot/Collector.java
+++ SwerveCode-Imported/src/main/java/frc/robot/Collector.java
@@ -2,17 +2,23 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+
 public class Collector {
 
-    Collector collector;
+    private DigitalInput lineSensorTop;
+    private DigitalInput lineSensorBottom;
     WPI_TalonFX processing;
     WPI_TalonFX staging;
+    public enum CollectorState{ONE_BALL_BOTTOM, ONE_BALL_TOP, TWO_BALLS, NO_BALLS_LOADED}
+    private CollectorState collectorState;
 
     //This class will contain all new collector specific methods
     public Collector(){
         WPI_TalonFX processing = new WPI_TalonFX(config_hw.newFlywheelCollector);
         WPI_TalonFX staging = new WPI_TalonFX(config_hw.newFlywheelStaging);
-        collector = new Collector();
+        lineSensorBottom = new DigitalInput(config_hw.LINE_BREAK_BOTTOM_SENSOR_PORT);
+        lineSensorTop = new DigitalInput(config_hw.LINE_BREAK_TOP_SENSOR_PORT);
     }
 
     //Drives the processing wheels for state machine
@@ -47,22 +53,69 @@ public class Collector {
 
     //Stops entire intake system if needed
     public void intakeAllStop(){
-        collector.stopProcessingWheels();
-        collector.stopStagingWheels();
+        this.stopProcessingWheels();
+        this.stopStagingWheels();
     }
 
     //This runs the intake system in its entirety if needed
     public void intakeAllRun(){
-        collector.driveProcessingWheels();
-        collector.driveStagingWheels();
+        this.driveProcessingWheels();
+        this.driveStagingWheels();
     }
 
     //reverses intake entirely in the event we need it
     public void intakeReverse(){
-        collector.reverseProcessingWheels();
-        collector.reverseStagingWheels();
+        this.reverseProcessingWheels();
+        this.reverseStagingWheels();
     }
 
-    
+    public CollectorState determineCollectorState(){
+        boolean topState = lineSensorTop.get();
+        boolean bottomState = lineSensorBottom.get();
+
+
+        //this makes sure to tell which states are which
+        if(bottomState){
+            if(topState){
+                collectorState = CollectorState.TWO_BALLS;
+            }else{
+                collectorState = CollectorState.ONE_BALL_BOTTOM;
+            }
+        }else{
+            if(topState){
+                collectorState = CollectorState.ONE_BALL_TOP;
+            }else{
+                collectorState = CollectorState.NO_BALLS_LOADED;
+            }
+        }
+
+
+        return collectorState;
+    }
+
+    public void ballControl(){
+        //this is a simple state machine controlling what ball wheels run and when
+        switch(this.determineCollectorState()){
+            case NO_BALLS_LOADED: //when there are no balls loaded we want to run the processing wheels to collect 1 ball
+                this.driveProcessingWheels();
+            break;
+          
+            case ONE_BALL_BOTTOM: //when there is one ball at the bottom we want to stop the wheels pushing it in and start driving the middle wheels
+                this.stopProcessingWheels();
+                this.driveStagingWheels();
+            break;
+            
+            case ONE_BALL_TOP: //when theres one ball at the top we want to make sure that the staging wheels don't move the ball and run the bottom wheels to collect another
+                this.stopStagingWheels();
+                this.driveProcessingWheels();
+            break;
+
+            case TWO_BALLS: //when we have 2 balls we don't want to run any of the intake modules
+                this.intakeAllStop();
+            break;
+        }
+    }
+
+
 
 }
