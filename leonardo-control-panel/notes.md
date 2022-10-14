304bc1ca9c2f792c0eb0dca44e1e2fa318e48d5f
Robert Marsh <rdmarsh2@gmail.com>
2022-10-14 12:59:21 -0400

    Arduino code for control panel

---
+++ leonardo-control-panel/notes.md
@@ -0,0 +1,7 @@
+
+## Report Format
+
+| Byte |7|6|5|4|3|2|1|0|
+|:------:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|
+|1| Switch 8 | Switch 7 | Button 6 | Button 5 | Button 4 | Button 3 | Button 2 | Button 1
+|2 | Dial |||| Switch 12 | Switch 11 | Switch 10 | Switch 9
---
+++ leonardo-control-panel/src/pins.h
@@ -0,0 +1,22 @@
+// block one - five connections plus ground. Lower buttons plus right small switch
+// placed here for ground pin.
+#define BUTTON_1 13
+#define BUTTON_2 12
+#define BUTTON_3 11
+#define BUTTON_4 10
+#define SWITCH_3 9
+
+// block two - five connections plus ground. middle row - switch/button/button/switch, plus left small switch
+#define SWITCH_1 4
+#define BUTTON_5 3
+#define BUTTON_6 2
+#define SWITCH_2 1
+#define SWITCH_4 0
+
+// block three - two switches. top row
+#define SWITCH_5 5
+#define SWITCH_6 6
+
+#define DIAL_PIN A0 // A0
+
+// TODO: Additional switches?
