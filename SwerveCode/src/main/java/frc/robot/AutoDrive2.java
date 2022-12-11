05d1516a61c37e181086491789498a1a796549fd
ClapperCR <clappecr23@pwcs-edu.org>
2022-12-11 16:25:36 -0500

    Testing smoothing filter and waypoint autodriving

--- SwerveCode/src/main/java/frc/robot/AutoDrive2.java
+++ SwerveCode/src/main/java/frc/robot/AutoDrive2.java
@@ -21,17 +21,19 @@ public class AutoDrive2 {
 
     PIDController pidVelocityControlX;
     PIDController pidVelocityControlY;
+    SmoothingFilter sf = new SmoothingFilter(5);
 
     private ArrayList<Pose2d> waypoints;
     /**
+     * Construct object and assign values
      * 
-     * @param kPX
-     * @param kDX
-     * @param kIX
-     * @param kPY
-     * @param kDY
-     * @param kIY
-     * @param maxV
+     * @param kPX P for X direction
+     * @param kDX D for X direction
+     * @param kIX I for X direction
+     * @param kPY P for Y direction
+     * @param kDY D for Y direction
+     * @param kIY I for Y direction
+     * @param maxV Max velocity the robot will move
      */
     public AutoDrive2(double kPX, double kDX, double kIX, 
                       double kPY, double kDY, double kIY, double maxV, double acceptanceRadius){
@@ -45,14 +47,15 @@ public class AutoDrive2 {
         pidVelocityControlX = new PIDController(kPX, kIX, kDX);
         pidVelocityControlY = new PIDController(kPY, kIY, kDY);
         this.acceptanceRadius = acceptanceRadius;
-        this.wayPoints = new ArrayList<Pose2d>();
+        this.waypoints = new ArrayList<Pose2d>();
     }
 
     /**
+     * Return speed needed to move to a certain position
      * 
-     * @param goal
-     * @param robot
-     * @return
+     * @param goal Position to move to
+     * @param robot Current position
+     * @return Velocities for each direction
      */
     public ChassisSpeeds moveTo(Pose2d goal, Pose2d robot) {
         double velocityX = 0;
@@ -64,18 +67,39 @@ public class AutoDrive2 {
           velocityY = Math.max(Math.min(velocityY, maxVelocity), -maxVelocity);
         }
 
-        return new ChassisSpeeds(velocityX, velocityY, 0);
+        //return new ChassisSpeeds(velocityX, velocityY, 0);
+        return sf.smooth(new ChassisSpeeds(velocityX, velocityY, 0));
     }
 
+    /**
+     * Distance formula
+     * 
+     * @param a Point A
+     * @param b Point B
+     * @return Distance between A and B
+     */
     public double getDistance(Pose2d a, Pose2d b) {
         return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2));
     }
+
+    /**
+     * Move to the next waypoint using moveTo method
+     * 
+     * @param robot Current position of robot
+     * @return Velocities for each direction
+     */
     public ChassisSpeeds moveToWayPoint(Pose2d robot) {
         if(!this.waypoints.isEmpty()){
             currentGoal = waypoints.remove(0);
         }
-        return this.moveTo(this.currentGoal, robot)
+        return this.moveTo(this.currentGoal, robot);
     }
+
+    /**
+     * Add a waypoint to the movement path
+     * 
+     * @param point Point to move to
+     */
     public void addWaypoint(Pose2d point){
         this.waypoints.add(point);
     }
---
+++ SwerveCode/src/main/java/frc/robot/SmoothingFilter.java
@@ -0,0 +1,38 @@
+package frc.robot;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+
+public class SmoothingFilter {
+    ChassisSpeeds[] velocityValues;
+    int size;
+    int index = 0;
+
+    /**
+     * Create smoothing object, will slowly and smoothly adjust speed values until target is hit
+     * @param size Size for smoothing array, bigger will be smoothed slower
+     */
+    public SmoothingFilter(int size) {
+        this.size = size;
+        velocityValues = new ChassisSpeeds[size];
+    }
+
+    /**
+     * Take an array of zeros and fill each slot with a speed value until the value is hit
+     * @param desiredSpeed Speed to accelerate towards
+     */
+    public ChassisSpeeds smooth(ChassisSpeeds desiredSpeed) {
+        double sumXVelocities = 0;
+        double sumYVelocities = 0;
+        velocityValues[index] = desiredSpeed;
+        for(ChassisSpeeds cs: velocityValues) {
+            if(!cs.equals(null)) {
+                sumXVelocities += cs.vxMetersPerSecond;
+                sumYVelocities += cs.vyMetersPerSecond;
+            }
+        }
+        index++;
+        index %= size;
+
+        return new ChassisSpeeds(sumXVelocities / size, sumYVelocities / size, 0);
+    }
+}
